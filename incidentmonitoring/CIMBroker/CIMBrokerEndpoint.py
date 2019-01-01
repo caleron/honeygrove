4b28b66f9b73efc0a28f1751f8067ee2a3b5042c
caleron <caleron@users.noreply.github.com>
2019-01-01 22:21:33 +0100

    log more stuff on the CIM broker endpoint

--- incidentmonitoring/CIMBroker/CIMBrokerEndpoint.py
+++ incidentmonitoring/CIMBroker/CIMBrokerEndpoint.py
@@ -1,7 +1,10 @@
+import atexit
 import base64
 import json
 import logging
+import socket
 import sys
+import threading
 import time
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
@@ -9,7 +12,6 @@ from logging.handlers import RotatingFileHandler
 import broker
 
 import CIMBroker.CIMBrokerConfig as CIMBrokerConfig
-import PrepareES
 from CIMBroker.CIMBrokerConfig import es
 from MHR.MalwareLookup import MalwareLookup
 
@@ -21,6 +23,8 @@ class CIMBrokerEndpoint:
     # "logs" and "files" are the Topics we subscribe.
     logsQueue = listenEndpoint.make_subscriber("logs")
     fileQueue = listenEndpoint.make_subscriber("files")
+    # also listen for events
+    endpointStatusEvents = listenEndpoint.make_status_subscriber(True)
 
     # setup logging for this endpoint
     # __name__ is the module name
@@ -42,6 +46,11 @@ class CIMBrokerEndpoint:
     fileHandler = RotatingFileHandler(filename=__name__ + '.log', maxBytes=1024 * 1024 * 10, backupCount=5)
     fileHandler.setFormatter(formatter)
     logger.addHandler(fileHandler)
+
+    logger.info("starting CIMBrokerEndpoint")
+
+    # count messages
+    messagesSinceLastStatusPrint = 0
 
     @staticmethod
     def connectEndpoints():
@@ -92,10 +101,11 @@ class CIMBrokerEndpoint:
 
         for (topic, data) in logQueue:
             for entry in [data]:
+                CIMBrokerEndpoint.messagesSinceLastStatusPrint = CIMBrokerEndpoint.messagesSinceLastStatusPrint + 1
                 CIMBrokerEndpoint.logger.info(entry)
 
                 # if connection to Elasticsearch is interrupted, cache logs into logs.json to prevent data loss.
-                if not PrepareES.check_ping():
+                if not CIMBrokerEndpoint.elasticsearch_reachable():
                     CIMBrokerEndpoint.logger.info(
                         "Elasticsearch unavailable. The logs will be saved in the logs.json under "
                         "/incidentmonitoring/ressources.")
@@ -131,3 +141,53 @@ class CIMBrokerEndpoint:
 
             CIMBrokerEndpoint.processMalwareFile(msgs1)
             CIMBrokerEndpoint.processLogFiles(msgs)
+
+            lsla = CIMBrokerEndpoint.endpointStatusEvents.poll()
+            for entry in lsla:
+                CIMBrokerEndpoint.logger.info("peering event: " + str(entry))
+
+    @staticmethod
+    def shutdown_log():
+        CIMBrokerEndpoint.logger.info("terminating endpoint")
+
+    @staticmethod
+    def log_messages_count():
+        """
+        Prints a message about the number of messages received since the last call. Honeygrove should send a heartbeat
+        every 60 seconds. Restarts itself after 5 minutes. This should make detection of connectivity issues or offline
+        honeypots easier.
+        """
+        if CIMBrokerEndpoint.messagesSinceLastStatusPrint > 0:
+            CIMBrokerEndpoint.logger.info(
+                str(CIMBrokerEndpoint.messagesSinceLastStatusPrint) + " messages in the last 5 minutes")
+
+        else:
+            CIMBrokerEndpoint.logger.info(
+                "No messages in the last 5 minutes. It seems no honeypot is currently active.")
+
+        # reset message count
+        CIMBrokerEndpoint.messagesSinceLastStatusPrint = 0
+        threading.Timer(300, CIMBrokerEndpoint.log_messages_count).start()
+
+    @staticmethod
+    def elasticsearch_reachable():
+        """
+        Checks if elasticsearch is reachable.
+        (The old implementation imported this function from PrepareES, which lead to circular dependencies)
+        """
+        # Check if Elasticsearch on port 9200 is reachable
+        from CIMBroker.CIMBrokerConfig import ElasticIp, ElasticPort
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = sock.connect_ex((ElasticIp, ElasticPort))
+        if result == 0:
+            pingstatus = True
+        else:
+            pingstatus = False
+            print('\033[91m' + "The connection to Elasticsearch is interrupted..." + '\033[0m')
+        return pingstatus
+
+
+# print a message when terminating this script
+atexit.register(CIMBrokerEndpoint.shutdown_log)
+# start logging message count
+threading.Timer(300, CIMBrokerEndpoint.log_messages_count).start()

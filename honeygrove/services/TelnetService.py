1af1b6f1e15db21293ee3ee714e3dd6aafdb0525
caleron <caleron@users.noreply.github.com>
2019-01-16 23:31:07 +0100

    updated telnet service to use new honey token database

--- honeygrove/services/TelnetService.py
+++ honeygrove/services/TelnetService.py
@@ -1,20 +1,22 @@
 # Telnet-Service
+
+from twisted.conch.telnet import StatefulTelnetProtocol, TelnetTransport
+from twisted.internet import protocol
 from twisted.internet import reactor
 
-from honeygrove.services.ServiceBaseModel import ServiceBaseModel, Limiter
 from honeygrove import config
-
-from twisted.conch.telnet import TelnetTransport, StatefulTelnetProtocol
-from twisted.internet import protocol
-
+from honeygrove.core.EsHoneytokenDB import EsHoneytokenDB
+from honeygrove.core.PasswordLists import PasswordLists
 from honeygrove.logging import log
+from honeygrove.logging.log import botmaster_login, log_message
+from honeygrove.services.ServiceBaseModel import ServiceBaseModel, Limiter
 
-import time
 
 class TelnetService(ServiceBaseModel):
+
     def __init__(self):
         super(TelnetService, self).__init__()
-        
+
         self._name = config.telnetName
         self._port = config.telnetPort
 
@@ -30,20 +32,62 @@ class TelnetService(ServiceBaseModel):
         self._stop = True
         self._transport.stopListening()
 
+
 class TelnetProtocol(StatefulTelnetProtocol):
+    honeytoken_db = EsHoneytokenDB(servicename=config.telnetName)  # type: EsHoneytokenDB
+    password_position_checker = PasswordLists(service=config.telnetName)  # type: PasswordLists
     state = "User"
+
+    def __init__(self):
+        self.peerOfAttacker = None
+        self.username = None
+        self.password = None
 
     def telnet_Password(self, line):
         self.password = line.decode("UTF-8")
 
         log.login(config.telnetName, self.peerOfAttacker, config.telnetPort, False, self.username, self.password, "")
 
-        time.sleep(2.0)
+        honey_password = self.honeytoken_db.get_honey_token(self.username)
+        if honey_password:
+            if honey_password == self.password:
+                # get access count from the IP
+                access_count = self.access_count_from_ip(self.peerOfAttacker, '1h')
+                # If a client uses a valid credential set on its first attempt, he is considered to be a botmaster
+                if access_count == 0:
+                    botmaster_login(config.telnetName, self.peerOfAttacker, config.telnetPort, self.username,
+                                    self.password)
+                    log_message("ALARM!!!! honey token used!!!!")
 
+            else:
+                self._write_fail_response()
+
+        else:
+            # no token exists
+            password_position = self.password_position_checker.get_lowest_password_position(self.password)
+
+            # do not allow passwords that are on the first 5 positions of known password lists
+            if password_position != -1 and password_position < 5:
+                return self._write_fail_response()
+
+            complexity = self.honeytoken_db.password_complexity(self.password)
+            log_message('password ' + self.password + ' has a complexity of ' + str(complexity))
+
+            if complexity > 0.5:
+                # password is sufficiently complex, grant access
+                self.honeytoken_db.save_honeytoken(self.username, self.password)
+                return self._write_success_response()
+
+    def _write_fail_response(self):
         response = "\nAuthentication failed\nUsername: "
         self.transport.write(response.encode("UTF-8"))
-
         self.state = "User"
+        return "Discard"
+
+    def _write_success_response(self):
+        response = "\nAuthentication succeeded\n$: "
+        self.transport.write(response.encode("UTF-8"))
+        # self.state = "Command" TODO what to do here
         return "Discard"
 
     def connectionMade(self):
@@ -56,6 +100,7 @@ class TelnetProtocol(StatefulTelnetProtocol):
         response = "Password: "
         self.transport.write(response.encode("UTF-8"))
         return "Password"
+
 
 class TelnetFactory(protocol.ServerFactory):
     protocol = lambda a: TelnetTransport(TelnetProtocol)
